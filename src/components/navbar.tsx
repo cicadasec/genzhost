8f0e3905295d44a264e01c4aa96e3172c8f718c9
Sahil Ansari <sahilpov@gmail.com>
2025-08-07 12:42:34 +0000

    make it box

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ import { Button } from './ui/button';
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border rounded-lg bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl flex-row w-full items-center justify-between select-none h-full">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
